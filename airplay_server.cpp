2ddd89d3ddd68098ac1c296af31634c9034209ed
dsafa22 <229340646@qq.com>
2019-05-23 19:58:41 +0800

    add win32 cmakelist

---
+++ airplay_server.cpp
@@ -0,0 +1,88 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 dsafa22, All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+#include <cstring>
+#include <signal.h>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stddef.h>
+#include "lib/raop.h"
+#include "lib/dnssd.h"
+#include "log.h"
+#include "lib/stream.h"
+#include "lib/logger.h"
+#include <malloc.h>
+#include <cstring>
+#include <math.h>
+#include "raop_server.h"
+
+extern "C" void
+audio_process(void *cls, void *opaque, pcm_data_struct *data)
+{
+    audio_session_t *session = (audio_session_t *)opaque;
+    for (int i = 0; i < data->data_len; i++) {
+        data->data[i] = data->data[i] * session->volume;
+    }   
+}
+
+
+extern "C" void
+video_process(void *cls, h264_decode_struct *data)
+{
+    
+}
+
+static bool is_running = false;
+void SignalHandler(int signal)
+{
+	switch (signal)
+	{
+	case SIGABRT:
+	case SIGTERM:
+		is_running = false;
+		break;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	typedef void (*SignalHandlerPointer)(int);
+	SignalHandlerPointer previousHandler;
+	char hw_addr[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+	int hw_addr_len = sizeof(hw_addr);
+
+	RaopServer* raop_server = new RaopServer("a", hw_addr, hw_addr_len);
+
+	if (raop_server->StartServer(NULL, audio_process, video_process) != 0) {
+		return 1;
+	}
+	LOGI("starting...");
+	is_running = true;
+	while (is_running) {
+		Sleep(100);
+	}
+	raop_server->StopServer();
+	return 0;
+}
