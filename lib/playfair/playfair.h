9a7219740fca623c5ba8c1af7568575f1ba4653d
dsafa22 <229340646@qq.com>
2019-05-24 21:34:34 +0800

    add cpp support

--- lib/playfair/playfair.h
+++ lib/playfair/playfair.h
@@ -1,6 +1,11 @@
 #ifndef PLAYFAIR_H
 #define PLAYFAIR_H
-
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 void playfair_decrypt(unsigned char* message3, unsigned char* cipherText, unsigned char* keyOut);
-
+#ifdef __cplusplus
+}
+#endif
 #endif
